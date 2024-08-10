54641d0f15efb8f7583730f7d04f7bda69458535
Hector Ledesma <hector.e.ledesma21@gmail.com>
2024-08-10 21:04:08 +0200

    Basic hello workld on '/'

---
+++ hello-world/src/main/java/com/emi/learns/hello_world/HelloController.java
@@ -0,0 +1,12 @@
+package com.emi.learns.hello_world;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!!!!!";
+    }
+}
